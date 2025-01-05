4a346ce43ce30bcec6477b46f845f43afa8eea4b
Yevheniy <Genya421@gmail.com>
2025-01-06 00:23:54 +0200

    add button

--- pages/🏡alliance.py
+++ pages/🏡alliance.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from load_data import data_alliance_power, data_alliance_merits
 
+if st.button("Clear All"):
+    st.cache_data.clear()
+
 st.sidebar.markdown("# Alliance statistic")
 st.title(" Alliance statistic")
 st.divider()
